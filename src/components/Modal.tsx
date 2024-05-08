296544086d5e47a1731c03129d52ff1bcb071da5
Donovan Allen <dallen.928@gmail.com>
2024-05-08 19:50:55 -0400

    refactor(components): modal updates

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -4,6 +4,8 @@ import * as Dialog from '@radix-ui/react-dialog';
 import React from 'react';
 import { IoMdClose } from 'react-icons/io';
 
+import IconButton from '@/components/buttons/IconButton';
+
 interface ModalProps {
   isOpen: boolean;
   onChange: (open: boolean) => void;
@@ -18,7 +20,6 @@ const Modal: React.FC<ModalProps> = ({
   onChange,
   title,
   description,
-  className,
   children,
 }) => {
   return (
@@ -27,12 +28,14 @@ const Modal: React.FC<ModalProps> = ({
         <Dialog.Overlay className='bg-neutral-900/90 backdrop-blur-sm fixed inset-0 z-10' />
         <Dialog.Content className='fixed drop-shadow-md border border-neutral-400 top-[50%] left-[50%] max-h-full h-full md:h-auto md:max-h-[85vh] w-full md:w-[90vw] md:max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-lg p-12 focus:outline-none z-10'>
           <Dialog.Close asChild>
-            <button className='text-neutral-400 hover:text-white absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:outline-none'>
-              <IoMdClose size={20} />
-            </button>
+            <IconButton
+              icon={IoMdClose}
+              variant='ghost'
+              className='text-neutral-400 text-lg hover:text-white absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded-full focus:outline-none'
+            />
           </Dialog.Close>
           {title && (
-            <Dialog.Title className='h1 text-center my-4'>{title}</Dialog.Title>
+            <Dialog.Title className='h1 text-center my-2'>{title}</Dialog.Title>
           )}
           {description && (
             <Dialog.Description className='subtitle mb-5 text-center'>
