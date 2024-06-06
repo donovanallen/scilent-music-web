de082cedcc46dba442bc4a0f84ac2fe0e3b389bf
Donovan Allen <dallen.928@gmail.com>
2024-06-06 18:55:20 -0400

    feat(components): adds show hide toggle

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -3,18 +3,22 @@
 // import { Avatar, Tooltip } from '@nextui-org/react';
 import { PlayHistory, Track, TrackItem } from '@spotify/web-api-ts-sdk';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useMediaQuery } from '@uidotdev/usehooks';
 import { usePathname } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 import React, { useEffect, useMemo, useState } from 'react';
 import { BiSearch } from 'react-icons/bi';
 import { HiHome } from 'react-icons/hi';
+import { RxCaretLeft, RxCaretRight } from 'react-icons/rx';
 import { TbMusicStar, TbUserHeart } from 'react-icons/tb';
 
 import sdk from '@/lib/spotify-sdk/ClientInstance';
+import { cn } from '@/lib/utils';
 import useAuthModal from '@/hooks/useAuthModal';
 
 import Box from '@/components/Box';
 import Button from '@/components/buttons/Button';
+import IconButton from '@/components/buttons/IconButton';
 import Feed from '@/components/Feed';
 import NextPill from '@/components/Pill';
 import SidebarItem from '@/components/SidebarItem';
@@ -28,6 +32,8 @@ interface SidebarProps {
 }
 
 const Sidebar: React.FC<SidebarProps> = ({ children }) => {
+  const isSmallDevice = useMediaQuery('only screen and (max-width : 769px)');
+
   const { currentTrack, setCurrentTrack } = useStore();
 
   const pathname = usePathname();
@@ -35,6 +41,7 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
   const authModal = useAuthModal();
   const queryClient = new QueryClient();
   const [history, setHistory] = useState<PlayHistory[] | null>();
+  const [sidebarOpen, setSidebarOpen] = useState<boolean>();
 
   const routes = useMemo(
     () => [
@@ -88,7 +95,12 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
 
   return (
     <div className='flex h-[100vh]'>
-      <div className='flex flex-col w-fit md:w-[300px] xl:w-[350px] gap-y-2 bg-black h-full p-2'>
+      <div
+        className={cn(
+          'flex flex-col w-fit md:w-[300px] xl:w-[350px] gap-y-2 bg-black h-full p-2 transition',
+          isSmallDevice ? (!sidebarOpen ? 'hidden' : '') : '',
+        )}
+      >
         {session ? (
           <>
             <Box>
@@ -137,6 +149,14 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
             <Box className='md:hidden flex flex-col h-[100%] w-full'>
               <Logo className='transform -rotate-90 align-middle origin-center my-auto' />
             </Box>
+            <Box className='md:hidden flex flex-col h-fit w-full'>
+              <IconButton
+                variant='primary'
+                icon={RxCaretLeft}
+                classNames={{ icon: 'text-3xl' }}
+                onClick={() => setSidebarOpen(!sidebarOpen)}
+              />
+            </Box>
           </>
         ) : (
           <Box className='h-full flex items-center justify-center '>
@@ -149,7 +169,30 @@ const Sidebar: React.FC<SidebarProps> = ({ children }) => {
           </Box>
         )}
       </div>
-      <main className='h-full flex-1 overflow-y-auto py-2'>
+      {isSmallDevice && (
+        <div
+          className={cn(
+            'h-full w-0 relative transition z-20',
+            !sidebarOpen
+              ? 'absolute left-0 z-10 hover:border-r-4 hover:border-brand-primary/70'
+              : 'hidden',
+          )}
+        >
+          <IconButton
+            variant='primary'
+            icon={RxCaretRight}
+            className='absolute bottom-12 left-[8px] z-10 transition'
+            classNames={{ icon: 'text-3xl transition' }}
+            onClick={() => setSidebarOpen(!sidebarOpen)}
+          />
+        </div>
+      )}
+      <main
+        className={cn(
+          'h-full flex-1 overflow-y-auto py-2',
+          !sidebarOpen ? 'relative' : '',
+        )}
+      >
         <QueryClientProvider client={queryClient}>
           {children}
         </QueryClientProvider>
