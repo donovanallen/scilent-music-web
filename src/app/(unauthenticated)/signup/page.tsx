57880a43b5d0d8727b1ae30c2f3e846ae9797c91
Donovan Allen <dallen.928@gmail.com>
2024-08-02 14:40:34 -0400

    feat(components): updates signup page layout and copy

--- src/app/(unauthenticated)/signup/page.tsx
+++ src/app/(unauthenticated)/signup/page.tsx
@@ -2,33 +2,42 @@
 
 import { Input } from '@nextui-org/react';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import React from 'react';
-import { FaChevronDown } from 'react-icons/fa6';
+import { PiAtom, PiMicrophone } from 'react-icons/pi';
+import { TbMusicHeart } from 'react-icons/tb';
+
+import { cn } from '@/lib/utils';
 
 import Box from '@/components/Box';
 import Button from '@/components/buttons/Button';
 
-import { cn } from '../../../lib/utils';
-
 import Logo from '~/images/Logo_3d.png';
-import Wordmark from '~/svg/Logo_Wordmark_White.svg';
+import Wordmark from '~/svg/Logo_Wordmark_Gray.svg';
+
+const features = [
+  {
+    title: 'Your Musical Universe',
+    description:
+      'Seamlessly aggregate your entire music history across all platforms, unlocking personalized insights and comprehensive artist discographies, including every new release.',
+    icon: <PiAtom size={64} />,
+  },
+  {
+    title: 'Share Your Rhythm',
+    description:
+      'Connect with friends and tastemakers through customizable music sharing, fostering a vibrant community of music lovers.',
+    icon: <TbMusicHeart size={64} />,
+  },
+  {
+    title: 'Reviews that Resonate',
+    description:
+      'Express your musical opinions with a tiered review system that adapts to your listening profile, featuring real-time reactions and in-depth critiques.',
+    icon: <PiMicrophone size={64} />,
+  },
+];
 
 export default function ArtsyLandingPage() {
   const [email, setEmail] = useState('');
-  const [mousePosition, setMousePosition] = useState({ x: 0, y: 0 });
-
-  useEffect(() => {
-    const handleMouseMove = (e: MouseEvent) => {
-      setMousePosition({ x: e.clientX, y: e.clientY });
-    };
-
-    window.addEventListener('mousemove', handleMouseMove);
-
-    return () => {
-      window.removeEventListener('mousemove', handleMouseMove);
-    };
-  }, []);
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
@@ -37,70 +46,98 @@ export default function ArtsyLandingPage() {
     setEmail('');
   };
 
-  function getFeatureDescription(feature: string): string {
-    switch (feature) {
-      case 'Connect':
-        return 'Find and connect with musicians, producers, and music lovers worldwide.';
-      case 'Create':
-        return 'Collaborate on tracks, share your music, and get feedback from the community.';
-      case 'Collaborate':
-        return 'Join virtual jam sessions, find bandmates, and grow your musical network.';
-      default:
-        return '';
-    }
-  }
+  const colors = ['#f9d3b4', '#d6d6d6', '#121212', '#756456']; // Gray, Red, Green
+  const [colorIndex, setColorIndex] = useState(0);
+  const cycleColor = () => {
+    setColorIndex((prevIndex) => (prevIndex + 1) % colors.length);
+  };
 
   return (
-    <Box className='flex flex-col w-full items-center overflow-y-auto overflow-x-hidden no-scrollbar scrollbar-hide'>
-      <div className='flex flex-col items-center justify-around h-screen p-12 relative'>
-        <div className='flex flex-col w-full h-fit space-y-12 items-center'>
-          <Image src={Logo} alt='Logo' width={500} height={500} />
-          <Wordmark className='max-w-10/12 shadow-lg' />
-          <h3 className='text-brand-light'>Where Music Meets Innovation</h3>
-        </div>
+    <Box className='flex flex-col items-center w-full bg-dark overflow-y-auto overflow-x-hidden relative'>
+      <div className='absolute inset-0 flex items-center justify-center h-screen'>
+        <Image
+          src={Logo}
+          alt='Background Logo'
+          layout='fill'
+          objectFit='cover'
+          quality={100}
+        />
+      </div>
 
-        <div className='grid grid-cols-1 md:grid-cols-3 gap-4 w-full max-w-4xl'>
-          {['Connect', 'Create', 'Collaborate'].map((title, index) => (
-            <Box
-              key={index}
-              className='bg-brand-dark bg-opacity-20 backdrop-filter backdrop-blur-sm p-4 rounded-lg transition-transform hover:-translate-y-1 hover:bg-opacity-30'
-            >
-              <h4 className='text-lg font-semibold mb-2'>{title}</h4>
-              <p>{getFeatureDescription(title)}</p>{' '}
-            </Box>
-          ))}
-        </div>
-        <div className='absolute bottom-0 left-1/2 -translate-x-1/2'>
-          <FaChevronDown className='text-brand-light w-8 h-8 animate-pulse' />
+      <div className='absolute inset-0 bg-gradient-to-b from-transparent to-dark opacity-95 h-full backdrop-invert backdrop-blur-lg' />
+
+      {/* LANDING HERO */}
+      <div className='relative z-10 h-screen w-full flex flex-col items-center'>
+        {/* Wordmark SVG */}
+        <div className='flex flex-col flex-1 items-center justify-center w-full'>
+          <Wordmark
+            className='w-10/12 drop-shadow-2xl cursor-pointer'
+            onClick={cycleColor}
+            style={{ fill: colors[colorIndex] }}
+          />
+          <h3 className='text-brand-primary font-semibold drop-shadow-xl mt-4'>
+            Be heard.
+          </h3>
         </div>
       </div>
 
-      <div className='bg-light/20 backdrop-filter backdrop-blur-md p-12 rounded-lg my-12 w-full max-w-lg shadow-lg flex flex-col items-center gap-y-4'>
-        <h3 className='text-brand-primary'>Join the Musical Revolution</h3>
-        <form
-          onSubmit={handleSubmit}
-          className='flex flex-col gap-y-2 items-center w-full'
-        >
-          <Input
-            type='email'
-            className='w-full'
-            radius='sm'
-            placeholder='Enter your email address'
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            aria-label='Email input'
-          />
-          <Button
-            type='submit'
-            variant='outline'
-            disabled={!email}
-            className={cn(
-              'w-full animated-underline text-center items-center justify-center',
-            )}
+      {/* FEATURES */}
+      <div className='relative z-10 h-screen w-full flex flex-col items-center justify-center gap-y-24 bg-gradient-to-b from-transparent to-dark '>
+        <h1 className='text-brand-light drop-shadow-xl'>
+          An elevated music experience
+        </h1>
+        {/* <div className='flex flex-1 items-center'> */}
+        <div className='grid grid-cols-1 md:grid-cols-3 gap-6 w-full px-24'>
+          {features.map(({ title, description, icon }, index) => (
+            <Box
+              key={index}
+              className='flex flex-col h-full bg-dark/50 p-6 rounded-lg transition-all duration-300 hover:-translate-y-2 hover:bg-opacity-30 hover:shadow-lg'
+            >
+              <div className='flex text-brand-light text-4xl mb-6'>{icon}</div>
+              <h3 className='mb-2 text-brand-primary'>{title}</h3>
+              <p className='text-light'>{description}</p>
+            </Box>
+          ))}
+        </div>
+        {/* </div> */}
+      </div>
+      <div className='relative z-10 h-screen w-full flex flex-col items-center justify-center bg-dark'>
+        <div className='rounded-lg max-w-xl shadow-lg flex flex-col items-center gap-y-12'>
+          <div>
+            <h4 className='text-light/60 w-full text-center'>
+              Beta coming 2024
+            </h4>
+            <h1 className='text-light w-full text-center'>
+              Join the Revolution
+            </h1>
+          </div>
+
+          <form
+            onSubmit={handleSubmit}
+            className='flex flex-col gap-y-4 items-center w-full'
           >
-            sign up
-          </Button>
-        </form>
+            <Input
+              type='email'
+              className='w-full text-light'
+              radius='sm'
+              size='lg'
+              placeholder='Email address'
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              aria-label='Email input'
+            />
+            <Button
+              type='submit'
+              variant='outline'
+              disabled={!email}
+              className={cn(
+                'w-full animated-underline text-center items-center justify-center transition-all duration-300',
+              )}
+            >
+              sign up
+            </Button>
+          </form>
+        </div>
       </div>
     </Box>
   );
